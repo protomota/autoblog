5d7758ea0a9525b1310be05a6a3e03f8fb25eabe
protomota <braddunlap@gmail.com>
2025-01-29 11:26:45 -0500

    removed test file

--- main.py
+++
@@ -1,9 +0,0 @@
-from dotenv import load_dotenv
-load_dotenv()
-
-import os
-project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-claude = os.getenv('ANTHROPIC_API_KEY')
-
-print(project_root)
-print(claude)
